ba31bf9b710532cbbf7b4ac0b62b02904d3ab56f
Jamie <jamie@mvcodeclub.com>
2020-01-28 00:42:05 -0800

    Running Animations

---
+++ Assets/Scenes/cameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform target;
+    public Collider2D roomBounds;
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, -10);
+        var h = camera.orthographicSize;
+        var w = h * Screen.width / Screen.height;
+        float x = Mathf.Clamp(transform.position.x, roomBounds.bounds.min.x + w, roomBounds.bounds.max.x - w);
+        float y = Mathf.Clamp(transform.position.y, roomBounds.bounds.min.y + h, roomBounds.bounds.max.y - h);
+        transform.position = new Vector3(x, y, -10);
+    }
+}
---
+++ Assets/Scenes/playerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int moveSpeed = 2;
+    BoxCollider2D collider;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        float hdir = Input.GetAxisRaw("Horizontal");
+
+        anim.SetBool("isRunning", hdir != 0);
+        if (hdir != 0)
+        {
+            translate(ref velocity, transform.right * hdir, moveSpeed);
+            transform.localScale = new Vector3(hdir, 1, 1);
+        }
+
+        transform.position += velocity;
+    }
+
+    Vector2 position2D()
+    {
+        return new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void translate(ref Vector3 vector, Vector3 direction, int pixels)
+    {
+        for (int s = pixels; s > 0; s--)
+        {
+            float dx = s / 32.0f;
+            RaycastHit2D boxcast = Physics2D.BoxCast(
+                position2D() + collider.offset,
+                collider.size * 0.95f,
+                0,
+                direction,
+                dx,
+                LayerMask.GetMask("Wall")
+            );
+
+            if (!boxcast)
+            {
+                vector += direction * dx;
+                s = 0;
+            }
+        }
+    }
+}
