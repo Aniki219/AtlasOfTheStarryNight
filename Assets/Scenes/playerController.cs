383e816a996a06ac4cfae8cd16b3be35935aa4e2
Jamie <jamie@mvcodeclub.com>
2020-01-29 23:53:23 -0800

    run and jump

--- Assets/Scenes/playerController.cs
+++ Assets/Scenes/playerController.cs
@@ -4,57 +4,92 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public int moveSpeed = 2;
-    BoxCollider2D collider;
+    [SerializeField] private int moveSpeed = 4;
+    [SerializeField] float fallRate = 23f;
+    float fallSpeed = 0;
     Animator anim;
+    new BoxCollider2D collider;
+
+    Vector3 velocity;
+    bool jump = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 velocity = Vector3.zero;
+
         float hdir = Input.GetAxisRaw("Horizontal");
 
         anim.SetBool("isRunning", hdir != 0);
         if (hdir != 0)
         {
-            translate(ref velocity, transform.right * hdir, moveSpeed);
+            translate(transform.right * hdir, moveSpeed * Time.fixedDeltaTime);
             transform.localScale = new Vector3(hdir, 1, 1);
         }
-
-        transform.position += velocity;
-    }
-
-    Vector2 position2D()
-    {
-        return new Vector2(transform.position.x, transform.position.y);
-    }
-
-    void translate(ref Vector3 vector, Vector3 direction, int pixels)
-    {
-        for (int s = pixels; s > 0; s--)
+        translate(transform.up * -1 * Mathf.Sign(fallSpeed), Mathf.Abs(fallSpeed * Time.fixedDeltaTime));
+        if (!isGrounded())
         {
-            float dx = s / 32.0f;
-            RaycastHit2D boxcast = Physics2D.BoxCast(
-                position2D() + collider.offset,
-                collider.size * 0.95f,
-                0,
-                direction,
-                dx,
-                LayerMask.GetMask("Wall")
-            );
-
-            if (!boxcast)
+            fallSpeed += fallRate * Time.fixedDeltaTime;
+        } else
+        {
+            if (fallSpeed > 0)
             {
-                vector += direction * dx;
-                s = 0;
+                fallSpeed = 0;
+            }
+
+            if (jump)
+            {
+                fallSpeed = -10f;
+                jump = false;
             }
         }
+    }
+
+    void translate(Vector3 direction, float dx)
+    {
+        RaycastHit2D boxcast = Physics2D.BoxCast(
+            (Vector2)(transform.position + direction * Time.fixedDeltaTime) + collider.offset,
+            collider.size * 0.95f,
+            0,
+            direction,
+            dx,
+            LayerMask.GetMask("Wall")
+        );
+
+        if (!boxcast)
+        {
+            transform.position += direction * dx;
+        } else
+        {
+            transform.position += boxcast.distance * direction;
+        }
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D boxcast = Physics2D.BoxCast(
+            (Vector2)transform.position + collider.offset,
+            collider.size * 0.95f,
+            0,
+            transform.up * -1,
+            Time.fixedDeltaTime,
+            LayerMask.GetMask("Wall")
+        );
+
+        return (bool)boxcast;
     }
 }
