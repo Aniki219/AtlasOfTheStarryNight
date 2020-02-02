8ed3d120be10979ecef79e68043bb16e65323daa
Jamie <jamie@mvcodeclub.com>
2020-02-02 00:28:16 -0800

    charactercontroller

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,19 +6,20 @@ public class cameraController : MonoBehaviour
 {
     public Transform target;
     public Collider2D roomBounds;
-    Camera camera;
+    Camera cam;
 
     void Start()
     {
-        camera = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, -10);
-        var h = camera.orthographicSize;
-        var w = h * Screen.width / Screen.height;
+        float h = cam.scaledPixelHeight/128.0f;
+        float w = cam.scaledPixelWidth/128.0f;
+
         float x = Mathf.Clamp(transform.position.x, roomBounds.bounds.min.x + w, roomBounds.bounds.max.x - w);
         float y = Mathf.Clamp(transform.position.y, roomBounds.bounds.min.y + h, roomBounds.bounds.max.y - h);
         transform.position = new Vector3(x, y, -10);
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (characterController))]
+public class playerController : MonoBehaviour
+{
+    public float jumpHeight = 2f;
+    public float timeToJumpApex = 0.5f;
+    public float airAccelerationTime = 0f;
+    public float groundAccelerationTime = 0f;
+    float moveSpeed = 4f;
+    int facing = 1;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    characterController controller;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<characterController>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.checkGrounded(ref velocity);
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Z) && isGrounded())
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        anim.SetBool("isRunning", isGrounded() && (targetVelocityX != 0));
+        anim.SetBool("isJumping", !isGrounded() && (velocity.y > 0));
+        anim.SetBool("isFalling", !isGrounded() && (velocity.y < -.25f));
+        anim.SetBool("isGrounded", isGrounded());
+
+        setFacing(targetVelocityX);
+        transform.localScale = new Vector3(facing, 1, 1);
+
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?groundAccelerationTime:airAccelerationTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void setFacing(float vel)
+    {
+        if (vel != 0)
+        {
+            facing = (int)Mathf.Sign(vel);
+        }
+    }
+
+    bool isGrounded()
+    {
+        return controller.collisions.isGrounded;
+    }
+}
