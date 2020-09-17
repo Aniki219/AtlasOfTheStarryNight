ece556cff3abd7747edf898610e8242d1e516a54
aniki219 <aniki219@gmail.com>
2020-09-16 23:23:54 -0700

    decorations

---
+++ Assets/Editor/Autosave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSave
+{
+    // Static constructor that gets called when unity fires up.
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += (PlayModeStateChange state) => {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
